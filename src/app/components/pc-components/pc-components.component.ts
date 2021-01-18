1e9458b8ff2fcdee751eed272ae34831b9e9a814
RadhikaSathe <radhikasathe1803@gmail.com>
2021-01-17 16:20:24 -0800

    Updated With product head and integration

--- src/app/components/pc-components/pc-components.component.ts
+++ src/app/components/pc-components/pc-components.component.ts
@@ -1,3 +1,8 @@
+import { PowersupplyService } from './../../services/powersupply.service';
+import { MotherBoardService } from './../../services/mother-board.service';
+import { GraphicsCard } from './../../common/products/graphics-card';
+import { GraphicCardService } from './../../services/graphic-card.service';
+import { CpucoolerService } from './../../services/cpucooler.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -6,6 +11,7 @@ import { CPU } from 'src/app/common/products/cpu';
 import { Observable } from 'rxjs';
 import { ProductConstants } from 'src/app/common/ProductConstants';
 import { DataService } from 'src/app/services/data.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 
 @Component({
@@ -20,7 +26,11 @@ export class PcComponentsComponent implements OnInit {
   data = [];
 
   type: String;
-  constructor(private productService: ProductService, private dataService: DataService) { }
+  constructor(private productService: ProductService,
+     private dataService: DataService,private cpuCooler:CpucoolerService ,
+     private graphicsCard:GraphicCardService,
+     private  motherBoard:MotherBoardService,
+     private powerSupply:PowersupplyService,private storage:StorageService) { }
 
   ngOnInit(): void {
     this.listProductCategories();
@@ -33,7 +43,18 @@ export class PcComponentsComponent implements OnInit {
     switch (this.type) {
       case ProductConstants.CORE_CPU:
         {
-          this.productHead = ["name", "core-count"]
+          this.productHead = ["name", "brand","color",
+          "coreClock",
+          "coreCount",
+          "graphicsType",
+         " integratedGraphics",
+          "manufacturer",
+
+          "price",
+          "simMultiThread",
+          "tdp",
+        "isAvailable"
+          ]
           this.productObser = this.productService.getProductList();
           // this.productObser.subscribe((p) => {
           //   console.log(p);
@@ -51,8 +72,10 @@ export class PcComponentsComponent implements OnInit {
       case ProductConstants.CORE_COOLER:
         {
 
-          this.productHead = ["name", "core-count"]
-          this.productObser = this.productService.getProductList();
+          this.productHead = ["fanRpm",
+            "noiseLevel" ,
+            "radiatorSize",]
+          this.productObser = this.cpuCooler.getProductList();
           // this.productObser.forEach(p => );
 
 
@@ -60,37 +83,59 @@ export class PcComponentsComponent implements OnInit {
         break;
       case ProductConstants.CORE_GPU:
         {
-
+          this.productHead = [
+           " chipset",
+            "videoMemory",
+           " coreClock",
+            "boostClock",
+            "length",
+           " thickNess"]
+          this.productObser = this.graphicsCard.getProductList();
         }
         break;
       case ProductConstants.CORE_MOBO:
         {
-
+          this.productHead = [ "maxMemory",
+            "memorySlots",
+            "chipset","socket"]
+          this.productObser = this.motherBoard.getProductList();
         }
         break;
       case ProductConstants.CORE_MONITOR:
         {
-
+          /* this.productHead = ["name", "core-count"]
+          this.productObser = this.productService.getProductList(); */
         }
         break;
       case ProductConstants.CORE_MONITOR:
         {
-
+          /* this.productHead = ["name", "core-count"]
+          this.productObser = this.productService.getProductList(); */
         }
         break;
       case ProductConstants.CORE_PSU:
         {
-
+          this.productHead = [
+           " formFactor",
+           " wattage"]
+          this.productObser = this.powerSupply.getProductList();
         }
         break;
       case ProductConstants.CORE_RAM:
         {
-
+          /* this.productHead = ["name", "core-count"]
+          this.productObser = this.productService.getProductList(); */
         }
         break;
       case ProductConstants.CORE_STORAGE:
         {
-
+          this.productHead = ["capacity",
+            "type",
+            "cache",
+            "formFactor",
+           " interFace",
+           " pricePerGb"]
+          this.productObser = this.storage.getProductList();
         }
         break;
 
